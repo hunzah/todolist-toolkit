bfe83afb87649daa731ab4310fc8b5bf1b042995
Yusup <hunzah9@mail.ru>
2023-07-25 12:58:09 +0300

    commit

--- src/app/store.ts
+++ src/app/store.ts
@@ -14,13 +14,15 @@ const rootReducer = combineReducers({
   app: appReducer,
   auth: authReducer,
 });
+export type RootReducerType = typeof rootReducer
 // непосредственно создаём store
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunkMiddleware),
 });
+
 // определить автоматически тип всего объекта состояния
-export type AppRootStateType = ReturnType<typeof rootReducer>;
+export type AppRootStateType = ReturnType<RootReducerType>;
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
