bb08e64197db20c3d9202eca31cdaf373d6bb493
Yusup <hunzah9@mail.ru>
2023-07-25 21:07:43 +0300

    commit

--- src/app/App.stories.tsx
+++ src/app/App.stories.tsx
@@ -15,3 +15,39 @@ export default {
 export const AppBaseExample = (props: any) => {
   return <App demo={true} />;
 };
+
+
+
+
+
+
+
+
+
+
+async function abc() {
+  console.log(8)
+
+  await Promise.resolve(2)
+      .then(console.log)
+
+  console.log(3)
+}
+
+setTimeout(() => {
+  console.log(1)
+}, 0)
+
+abc()
+
+queueMicrotask(() => {
+  console.log(0)
+})
+
+Promise.resolve(4)
+    .then(console.log)
+
+console.log(6)
+
+
+
