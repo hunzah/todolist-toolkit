cade2605aa5a87f82c6d9e5bcbdca44be25fb5ef
Yusup <hunzah9@mail.ru>
2023-07-29 21:28:04 +0300

    commit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,25 +1,25 @@
-import { tasksReducer } from "../features/TodolistsList/tasks-reducer";
-import { todolistsReducer } from "../features/TodolistsList/todolists-reducer";
-import { combineReducers } from "redux";
-import thunkMiddleware from "redux-thunk";
-import { appReducer } from "./app-reducer";
-import { authReducer } from "../features/Login/auth-reducer";
-import { configureStore } from "@reduxjs/toolkit";
+import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
+import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
+import {combineReducers} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import {appReducer} from './app-reducer';
+import {authReducer} from '../features/Login/auth-reducer';
+import {configureStore} from '@reduxjs/toolkit';
 import {useDispatch} from 'react-redux';
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
 const rootReducer = combineReducers({
-  tasks: tasksReducer,
-  todolists: todolistsReducer,
-  app: appReducer,
-  auth: authReducer,
+    tasks: tasksReducer,
+    todolists: todolistsReducer,
+    app: appReducer,
+    auth: authReducer,
 });
 export type RootReducerType = typeof rootReducer
 // непосредственно создаём store
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunkMiddleware),
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunkMiddleware),
 });
 
 // определить автоматически тип всего объекта состояния
@@ -30,4 +30,4 @@ export type AppRootStateType = ReturnType<RootReducerType>;
 window.store = store;
 
 export type AppDispatchType = typeof store.dispatch;
-export const useAppDispatch = ()=> useDispatch<AppDispatchType>();
+export const useAppDispatch = () => useDispatch<AppDispatchType>();
