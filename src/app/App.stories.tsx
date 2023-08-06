4e8fc99e3eb8bd29692701dc12437280aa2c93c6
Yusup <hunzah9@mail.ru>
2023-08-06 10:12:57 +0300

    19 - rtk - thunks

--- src/app/App.stories.tsx
+++ src/app/App.stories.tsx
@@ -1,54 +1,15 @@
-import React from "react";
-import { action } from "@storybook/addon-actions";
-import App from "./App";
-import {
-  BrowserRouterProviderDecorator,
-  ReduxStoreProviderDecorator
-} from '../stories/decorators/ReduxStoreProviderDecorator';
+import React from 'react'
+import {action} from '@storybook/addon-actions'
+import App from './App'
+import {BrowserRouterDecorator, ReduxStoreProviderDecorator} from '../stories/decorators/ReduxStoreProviderDecorator'
+import StoryRouter from 'storybook-react-router';
 
 export default {
-  title: "App Stories",
-  component: App,
-  decorators: [ReduxStoreProviderDecorator,BrowserRouterProviderDecorator],
-};
-
-
-export const AppBaseExample = (props: any) => {
-  return <App demo={true} />;
-};
-
-
-
-
-
-
-
-
-
-
-async function abc() {
-  console.log(8)
-
-  await Promise.resolve(2)
-      .then(console.log)
-
-  console.log(3)
+    title: 'Application Stories',
+    component: App,
+    decorators: [ReduxStoreProviderDecorator, BrowserRouterDecorator]
 }
 
-setTimeout(() => {
-  console.log(1)
-}, 0)
-
-abc()
-
-queueMicrotask(() => {
-  console.log(0)
-})
-
-Promise.resolve(4)
-    .then(console.log)
-
-console.log(6)
-
-
-
+export const AppBaseExample = (props: any) => {
+    return (<App demo={true} />)
+}
